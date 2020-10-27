e463b30a7960d904dc4801a477085296eae4317e
Leon <leon.karho.chung@gmail.com>
2020-10-27 16:28:04 -0400

    login forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {useForm} from 'react-hook-form'
 
 const Login = () => {
+  const {register, handleSubmit, watch, errors} = useForm()
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+  
   return (
     <div>
-      <h1>this is where user will login</h1>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input ref={register} name='email' type="text" placeholder='Email'/>
+        <input ref={register({required:true})} name='password' type="password" placeholder='Password'/>
+        {errors.password && <span>Password field is required</span>}
+        <input type="submit"/>
+      </form>
       <Link to='/register'>Register</Link>
     </div>
   )
